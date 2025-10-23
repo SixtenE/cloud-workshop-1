d8c360a7b259cc36932018977f7b27456e826f2a
SixtenE <sixten.ekblad@gmail.com>
2025-10-23 11:33:48 +0200

    add checkbox and toggle function

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { storage } from "@/lib/storage";
 import { createClient } from "redis";
+import { Todo } from "@/lib/types";
 
 export async function DELETE(
   request: NextRequest,
@@ -17,4 +18,29 @@ export async function DELETE(
   await redis.del(id);
 
   return NextResponse.json({ success: true });
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  if (!id) {
+    return NextResponse.json({ error: "ID is required" }, { status: 400 });
+  }
+
+  const redis = await createClient({ url: process.env.REDIS_URL }).connect();
+  const todoString = await redis.get(id);
+  const existingTodo: Todo | null = todoString ? JSON.parse(todoString) : null;
+
+  if (!existingTodo) {
+    return NextResponse.json({ error: "Todo not found" }, { status: 404 });
+  }
+
+  const newTodo: Todo = { ...existingTodo, completed: !existingTodo.completed };
+
+  await redis.set(id, JSON.stringify(newTodo));
+
+  return NextResponse.json(newTodo);
 }
